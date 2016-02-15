2d957e15b5197d2df82b81601eff6a26c5de9d17
Dmitry Stremous <stremous@list.ru>
2016-02-15 21:26:27 +0300

    Team work: continue working on profile logic

--- app/profile/profile.controller.js
+++ app/profile/profile.controller.js
@@ -1,9 +1,21 @@
 (function(){
   'use strict';
+
   angular
     .module('stimulApp')
     .controller('profileCtrl', editProfile);
-    function editProfile ($scope){
-      $scope.buttonValue = 'edit';
+
+    function editProfile ($scope, $state){
+      $scope.buttonValue = 'Edit';
+
+      $scope.profile = function(buttonValue) {
+        if (buttonValue === 'Edit'){
+          $scope.buttonValue = 'Save';
+          $state.go('profile/zzz/edit', {userProved: true});
+        } else if(buttonValue === 'Save') {
+            $scope.buttonValue = 'Edit';
+            $state.go('profile/zzz', {userProved: true});
+        }
+      };
     }
 })();
