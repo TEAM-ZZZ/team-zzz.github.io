f1ffd4f152410fef430aac590b87df0d32e49dbd
Dmitry Stremous <stremous@list.ru>
2016-02-15 18:26:51 +0300

    create config

--- app/app.config.js
+++ app/app.config.js
@@ -6,6 +6,19 @@ angular
   .config(config);
 
 function config($stateProvider, $urlRouterProvider) {
-
+  $stateProvider
+    .state('homePage', {
+      url: '/home',
+      templateUrl: 'app/login/login.html',
+      controller: 'loginCtrl'
+    })
+    .state('personalPage', {
+      url: '/profile',
+      templateUrl: 'app/profile/profile.html',
+      controller: 'profileCtrl'
+    });
+  $urlRouterProvider
+    .otherwise('/home');
+}
 
 })();
