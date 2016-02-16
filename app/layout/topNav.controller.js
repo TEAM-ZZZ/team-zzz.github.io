4d6daeea54e718e141d1c361a40768fdcbfa4d5f
KrystsinaKastsiuk <kristina.v.kostyuk@gmail.com>
2016-02-16 14:15:41 +0300

    Update topNav logic

--- app/layout/topNav.controller.js
+++ app/layout/topNav.controller.js
@@ -4,7 +4,9 @@
     .module('stimulApp')
     .controller('topNavCtrl', topNav);
 
-  function topNav ($scope){
-
-  }
+		function topNav ($scope, $state){
+			$scope.isNav = true;
+			if ($state.current.url.substr(1, 7) === "profile")
+			{$scope.isNav = false;}
+		}
 })();
