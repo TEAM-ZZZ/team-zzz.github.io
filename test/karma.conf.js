3f6eb717938a6a1f4b3a7a8ab30d99a95e6ec7cc
Dmitry Stremous <stremous@list.ru>
2016-02-16 15:31:12 +0300

    a try to set tests

---
+++ test/karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'bower/angular/angular.js',
+      'bower/angular-ui-route/angular-ui-route.js',
+      'bower/angular-mocks/angular-mocks.js',
+      'app/*.js',
+      'app/**/*.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome', 'Firefox'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
