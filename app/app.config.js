1e09aa312527433351947b8337092d51599d3571
Dmitry Stremous <stremous@list.ru>
2016-02-18 16:47:47 +0300

    Team Work: rectify code

--- app/app.config.js
+++ app/app.config.js
@@ -15,18 +15,7 @@ function config($stateProvider, $urlRouterProvider) {
     .state('personalPage', {
       url: '/profile/zzz',
       templateUrl: 'app/profile/profile.html',
-      controller: 'profileCtrl',
-      params: {
-        userProved: false
-      }
-    })
-    .state('personalPageEdit', {
-      url: '/profile/zzz/edit',
-      templateUrl: 'app/profile-edit/profile-edit.html',
-      controller: 'profileEditCtrl',
-      params: {
-        userProved: false
-      }
+      controller: 'profileCtrl'
     })
   $urlRouterProvider
     .otherwise('/home');
