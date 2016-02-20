680610b5a29f2cbbc32e1e90209102ec011dc962
Dmitry Stremous <stremous@list.ru>
2016-02-20 17:05:41 +0300

    add logic to messages page

--- src/app/history/history.controller.js
+++ src/app/history/history.controller.js
@@ -5,7 +5,16 @@
     .module('stimulApp')
     .controller('historyCtrl', historyCtrl);
 
-    function historyCtrl ($scope, $state, userService){
+    function historyCtrl ($scope, $state, $http){
+      $http.get('src/assets/fakeData/history.json').then(success, error);
 
+
+      function success(answer) {
+        $scope.historyData = answer.data;
+        console.log($scope.historyData);
+      }
+      function error(error) {
+        console.log("Something goes wrong", error);
+      }
     }
 })();
--- src/app/messages/messages.controller.js
+++ src/app/messages/messages.controller.js
@@ -5,7 +5,17 @@
     .module('stimulApp')
     .controller('messagesCtrl', messagesCtrl);
 
-    function messagesCtrl ($scope, $state, userService){
+    function messagesCtrl ($scope, $state, $http){
+      $http.get('src/assets/fakeData/messages.json').then(success, error);
+      $scope.messagesData = {};
 
+      function success(answer) {
+        $scope.messagesData = answer.data;
+        console.log($scope.messagesData);
+      }
+      function error(error) {
+        console.log("Something goes wrong", error);
+      }
+      $('.collapsible').collapsible({accordion : false});
     }
 })();
