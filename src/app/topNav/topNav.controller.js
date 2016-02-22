36a5f2bb43464318eb8f2da9bf125692e4b7bc55
Dmitry Stremous <stremous@list.ru>
2016-02-22 18:37:45 +0300

    Team Work(Polina and Dima): modify topNav (get userData for profile data)

--- src/app/topNav/topNav.controller.js
+++ src/app/topNav/topNav.controller.js
@@ -4,11 +4,21 @@
     .module('stimulApp')
     .controller('topNavCtrl', topNav);
 
-  function topNav($scope, $state){
-    console.log($state);
+  function topNav($scope, $state, httpService){
     $scope.showLoginNav = $state.params.showLoginNav || false;
     $scope.showProfileNav = $state.params.showProfileNav || false;
 
+    if(httpService.userData != null){
+      $scope.userData = httpService.userData;
+    } else {
+      httpService.getUser().then(success, error);
+    }
+    function success(answer) {
+      $scope.userData = answer.data;
+    }
+    function  error(reason) {
+      console.log('Sorry, something went wrong. The source data is unavailable.' + reason)
+    }
     $('.button-collapse').sideNav({
       closeOnClick: true
     });
