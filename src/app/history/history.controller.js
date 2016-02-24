d6ee6fdf5b8afef2722535df113aa33e3fa624ca
KrystsinaKastsiuk <kristina.v.kostyuk@gmail.com>
2016-02-25 01:43:43 +0300

    update controller history

--- src/app/history/history.controller.js
+++ src/app/history/history.controller.js
@@ -6,17 +6,20 @@ angular
   .controller('historyCtrl', historyCtrl);
 
   function historyCtrl($scope, $state, httpService) {
-    $scope.sortType = 'name';
-    $scope.sortReverse = false;
+    $scope.sortBack = false;
     $scope.search  = '';
+		
+		$scope.sort = function(name){
+			$scope.sortType === name;
+			$scope.sortBack = !$scope.sortBack;
+		}
 
-   
-      httpService.getHistory().then(success, error);
+    httpService.getHistory().then(success, error);
     
-
     function success(responce) {
       $scope.historyData = responce.data;
     }
+		
     function error(error) {
       console.log("Something goes wrong", error);
     }
