207cc9ddfdade775858bdb63d1bcbae3d09ef62c
Dmitry Stremous <stremous@list.ru>
2016-02-25 19:01:50 +0300

    change from fake to real authorization

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -9,36 +9,36 @@
     $scope.showError = false;
 
     $scope.isValid = function(login, password) {
-     /* $http.get([
+     $http.get([
           'http://fathomless-everglades-3680.herokuapp.com/api/login',
           '?password=',
           password,
           '&login=',
           login
         ].join(''))
-          .then(loginSuccess, loginError);*/
+          .then(loginSuccess, loginError);
 
       //temporary for fake request
-      $http.get([
+      /*$http.get([
         'src/assets/fakeData/auth.json',
         '?password=',
         password,
         '&login=',
         login
       ].join(''))
-        .then(loginSuccess, loginError);
+        .then(loginSuccess, loginError);*/
 
       $('.preloader-wrapper.big').toggleClass('active');
       $('.login-form input, .login-form button').attr('disabled', true);
 
       function loginSuccess(response) {
         //temporary check for fake request
-        if ((login !== "admin") || (password !== "123")) {
+        /*if ((login !== "admin") || (password !== "123")) {
           loginError("wrong login or password");
           return false;
-        }
+        }*/
 
-        $timeout(function() {
+        if (response.data.success) {
           $('.preloader-wrapper.big').toggleClass('active');
           $('.login-form input, .login-form button').removeAttr('disabled');
           $('#loginWindow').closeModal();
@@ -46,16 +46,21 @@
 
           authFactory.setAccessToken(response.data.token);
           $state.go('root.profile');
-        },1500);
-      }
-      function loginError(response) {
-        $timeout(function() {
+        } else {
           $('.preloader-wrapper.big').toggleClass('active');
           $('.login-form input, .login-form button').removeAttr('disabled');
 
-          console.log(response);
+          console.log("wrong login or password");
           $scope.showError = 'true';
-        },1500);
+        }
+
+      }
+      function loginError(response) {
+        $('.preloader-wrapper.big').toggleClass('active');
+        $('.login-form input, .login-form button').removeAttr('disabled');
+
+        console.log(response);
+        $scope.showError = 'true';
       }
     };
 
